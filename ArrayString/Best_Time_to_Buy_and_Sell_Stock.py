fb818350844551a500a85c9d453f27bc47541bbe
Ali Boudaghi <ali@smtp.iran.liara.ir>
2025-01-02 21:11:32 +0330

    solved

---
+++ ArrayString/Best_Time_to_Buy_and_Sell_Stock.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maxProfit(self, prices: list) -> int:
+        result = 0
+        max_index = 0
+        i = 0
+        while i < len(prices):
+            maximum = max(prices[i:])
+            max_index = prices.index(maximum, i)
+            print(max_index)
+            minimum = min(prices[:max_index + 1])
+            if maximum - minimum > result:
+                result = maximum - minimum
+            i = max_index + 1
+        return result
+
+        #min_price = prices[0]
+        #max_profit = 0
+        #
+        #for price in prices[1:]:
+        #    max_profit = max(max_profit, price - min_price)
+        #    min_price = min(min_price, price)
+        #    
+        #return max_profit
+
+nums = [7, 1, 5, 3, 6, 4]
+solution = Solution()
+print(solution.maxProfit(nums))
